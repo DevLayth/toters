612394ced80947c5b52a198873cb4b4cca25e562
Layth <layth.hikmat20@gmail.com>
2023-03-03 14:10:11 +0300

    clear home screen

--- lib/layth/home.dart
+++ lib/layth/home.dart
@@ -2,17 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 
+import 'package:toters/main.dart';
+
 class home_screen extends StatelessWidget {
   const home_screen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Center(child: Text("Layth")),
-        ),
-      ),
+      home: Scaffold(),
     );
   }
 }
