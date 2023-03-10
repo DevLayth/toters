f221cfb2e22c9776599390cf305f3dba815a8241
Layth <layth.hikmat20@gmail.com>
2023-03-10 14:44:39 +0300

    add data to modul

--- lib/layth/rest_data_modul.dart
+++ lib/layth/rest_data_modul.dart
@@ -1,10 +1,25 @@
 import 'package:toters/layth/rest_card.dart';
 
-List<rest_card> rest_data = [
+const List<rest_card> rest_data = [
   rest_card(
     name: "Texas - Duhok",
-    Type: "\$\$\$ Fast Food",
+    Type: "\$\$\$. Fast Food",
     dliver: "18-30",
     img: "assets/rest/texas.png",
-  )
+    rate: 4.5,
+  ),
+  rest_card(
+    name: "Tandori",
+    Type: "\$\$. Fast Food",
+    dliver: "20-45",
+    img: "assets/rest/texas.png",
+    rate: 3.8,
+  ),
+  rest_card(
+    name: "Shagaf - Duhok",
+    Type: "\$. Coffe",
+    dliver: "15-25",
+    img: "assets/rest/texas.png",
+    rate: 4.7,
+  ),
 ];
