3b80925e5d779881d2edd57939c1fe110a78f75a
Maryam Esam <m0ry0mes0mkh0lid@gmail.com>
2023-03-11 21:54:25 +0300

    create green pts card .

--- lib/maryam/account_screen.dart
+++ lib/maryam/account_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:toters/colors.dart';
+import 'package:toters/maryam/green_pts_card.dart';
 
 class acoount_screen extends StatelessWidget {
   const acoount_screen({super.key});
@@ -11,12 +12,17 @@ class acoount_screen extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
           appBar: AppBar(
-        backgroundColor: Colors.white,
-        title: Text(
-          "ِAccount Name",
-          style: TextStyle(color: Colors.black),
-        ),
-      )),
+            backgroundColor: Colors.white,
+            title: Text(
+              "ِAccount Name",
+              style: TextStyle(color: Colors.black),
+            ),
+          ),
+          body: ListView(
+            children: [
+              green_pts_card(),
+            ],
+          )),
     );
   }
 }
---
+++ lib/maryam/green_pts_card.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:toters/colors.dart';
+
+class green_pts_card extends StatelessWidget {
+  const green_pts_card({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 100,
+      child: Stack(
+        children: [
+          Positioned(
+            top: 20,
+            left: 25,
+            child: Container(
+              height: 60,
+              width: 250,
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(50),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.3),
+                      spreadRadius: 2,
+                      blurRadius: 5,
+                      offset: Offset(0, 3),
+                    ),
+                  ]),
+            ),
+          ),
+          Positioned(
+            top: 30,
+            left: 90,
+            child: Text(
+              "Green",
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+            ),
+          ),
+          Positioned(
+              top: 50,
+              left: 90,
+              child: Text(
+                "0 pts",
+                style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.black45,
+                    fontWeight: FontWeight.bold),
+              )),
+          Positioned(
+            top: 25,
+            left: 40,
+            child: Icon(
+              Icons.star_purple500,
+              size: 40,
+              color: Tcolor,
+            ),
+          ),
+          Positioned(
+              top: 37,
+              left: 240,
+              child: Icon(
+                Icons.arrow_forward_ios,
+                color: Tcolor,
+              ))
+        ],
+      ),
+    );
+  }
+}
