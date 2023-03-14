4c8dd747547c79c80864cefacfeac600e11ecebc
Layth <layth.hikmat20@gmail.com>
2023-03-14 22:23:33 +0300

    create data module and json file

---
+++ lib/layth/resturant_data_fetch.dart
@@ -0,0 +1,34 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import 'data_classes.dart';
+
+Future<List<Restaurant>> fetchRestaurants() async {
+  final jsonString = await rootBundle.loadString('assets/restaurants.json');
+
+  final List<dynamic> jsonList = json.decode(jsonString);
+
+  List<Restaurant> restaurants = jsonList.map((json) {
+    final List<dynamic> menuList = json['rmenu'];
+    List<MenuItem> menu = menuList.map((menuJson) {
+      return MenuItem(
+        id: menuJson['fid'],
+        name: menuJson['fname'],
+        price: menuJson['fprice'],
+      );
+    }).toList();
+
+    return Restaurant(
+      id: json['rid'],
+      name: json['rname'],
+      image: json['rimg'],
+      type: json['rtype'],
+      desc: json['rdesc'],
+      diliver: json['rdliver'],
+      menu: menu,
+    );
+  }).toList();
+
+  return restaurants;
+}
