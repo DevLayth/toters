fc1c1b56c77bff1d6968d84e2366f01cf17a72cb
Maryam Esam <m0ry0mes0mkh0lid@gmail.com>
2023-03-15 15:27:11 +0300

    create order screen .

--- lib/maryam/order_screen.dart
+++ lib/maryam/order_screen.dart
@@ -1,11 +1,39 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:toters/maryam/order_card.dart';
 
 class order_screen extends StatelessWidget {
   const order_screen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          title: Text(
+            " Orders",
+            style: TextStyle(color: Colors.black),
+          ),
+        ),
+        body: Stack(
+          children: [
+            Positioned(
+                left: 20,
+                top: 20,
+                child: Text(
+                  "Past Orders",
+                  style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+                )),
+            Container(
+              child: ListView(
+                children: [order_card(), order_card()],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
