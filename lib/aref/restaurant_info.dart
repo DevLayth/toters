fd290df35d45e216bb97ad7fe15fd6f4f0508f9e
arefdusky <hih4354@gmail.com>
2023-03-17 19:25:22 +0300

    change some part of cose

--- lib/aref/restaurant_info.dart
+++ lib/aref/restaurant_info.dart
@@ -40,43 +40,7 @@ class RestaurantInfo extends StatelessWidget {
                 ),
               ),
             ),
-            Row(
-              children: [
-                OutlinedButton(
-                  onPressed: () {},
-                  style: OutlinedButton.styleFrom(
-                    primary: const Color(0xFF22A45D),
-                    fixedSize: const Size(120, 40),
-                    side: const BorderSide(color: Color(0xFF22A45D)),
-                    shape: const RoundedRectangleBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(8)),
-                    ),
-                  ),
-                  child: Text(
-                    "Take away".toUpperCase(),
-                    style: const TextStyle(fontSize: 12),
-                  ),
-                ),
-                OutlinedButton(
-                  onPressed: () {},
-                  style: OutlinedButton.styleFrom(
-                    primary: Color.fromARGB(255, 16, 108, 212),
-                    fixedSize: const Size(120, 40),
-                    side: const BorderSide(
-                        color: Color.fromARGB(255, 17, 164, 201)),
-                    shape: const RoundedRectangleBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(8)),
-                    ),
-                  ),
-                  child: Text(
-                    "earn pints".toUpperCase(),
-                    style: const TextStyle(fontSize: 12),
-                  ),
-                ),
-                const SizedBox(width: 8),
-                const SizedBox(width: 4),
-              ],
-            ),
+
             const Spacer(),
             Row(
               children: [
