52d917ed4c21893e3333280f2423624f6559a03e
Maryam Esam <m0ry0mes0mkh0lid@gmail.com>
2023-03-19 21:26:30 +0300

    Add rate star .

--- lib/maryam/order_card_clicked.dart
+++ lib/maryam/order_card_clicked.dart
@@ -4,6 +4,7 @@ import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:flutter/widgets.dart';
 import 'package:toters/colors.dart';
 import 'package:toters/maryam/order_list.dart';
+import 'package:toters/maryam/rate_star.dart';
 
 class order_card_clicked extends StatelessWidget {
   final String name;
@@ -20,13 +21,28 @@ class order_card_clicked extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListView(
+      shrinkWrap: true,
+      physics: NeverScrollableScrollPhysics(),
       children: [
         Padding(
           padding: const EdgeInsets.all(15.0),
           child: Container(
-              height: 350,
-              color: Colors.white,
+              height: 360,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: Colors.white,
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.3),
+                    spreadRadius: 2,
+                    blurRadius: 5,
+                    offset: Offset(0, 3),
+                  ),
+                ],
+              ),
               child: ListView(
+                shrinkWrap: true,
+                physics: NeverScrollableScrollPhysics(),
                 children: [
                   Stack(
                     children: [
@@ -75,8 +91,14 @@ class order_card_clicked extends StatelessWidget {
                       ),
                     ],
                   ),
+                  Divider(
+                    thickness: 1,
+                    color: Color.fromARGB(129, 197, 192, 192),
+                    indent: 25,
+                    endIndent: 25,
+                  ),
                   Container(
-                      height: 80,
+                      height: 60,
                       child: Stack(
                         children: [
                           Positioned(
@@ -104,8 +126,14 @@ class order_card_clicked extends StatelessWidget {
                                       fontWeight: FontWeight.bold)))
                         ],
                       )),
+                  Divider(
+                    thickness: 1,
+                    color: Color.fromARGB(129, 197, 192, 192),
+                    indent: 25,
+                    endIndent: 25,
+                  ),
                   Container(
-                    height: 80,
+                    height: 50,
                     child: Stack(
                       children: [
                         Positioned(
@@ -125,33 +153,104 @@ class order_card_clicked extends StatelessWidget {
                       ],
                     ),
                   ),
-                  Container(
-                    child: Row(
-                      children: [
-                        ElevatedButton.icon(
-                            style: ElevatedButton.styleFrom(
-                              backgroundColor: Colors.white,
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(10.0),
-                                side: BorderSide(color: Tcolor),
+                  Divider(
+                    thickness: 1,
+                    color: Color.fromARGB(129, 197, 192, 192),
+                    indent: 25,
+                    endIndent: 25,
+                  ),
+                  Padding(
+                    padding:
+                        const EdgeInsets.only(left: 15, right: 15, top: 10),
+                    child: Container(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          ElevatedButton.icon(
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: Colors.white,
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(10.0),
+                                  side: BorderSide(color: Tcolor),
+                                ),
+                                minimumSize: Size(160, 50),
                               ),
-                              minimumSize: Size(150, 50),
-                            ),
-                            onPressed: () {},
-                            icon: Icon(
-                              Icons.support_agent_outlined,
-                              color: Tcolor,
-                            ),
-                            label: const Text(
-                              "Support",
-                              style: TextStyle(color: Tcolor),
-                            ))
-                      ],
+                              onPressed: () {},
+                              icon: Icon(
+                                Icons.support_agent_outlined,
+                                color: Tcolor,
+                              ),
+                              label: const Text(
+                                "Support",
+                                style: TextStyle(color: Tcolor),
+                              )),
+                          ElevatedButton.icon(
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: Tcolor,
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(10.0),
+                                  side: BorderSide(color: Tcolor),
+                                ),
+                                minimumSize: Size(160, 50),
+                              ),
+                              onPressed: () {},
+                              icon: Icon(
+                                Icons.replay,
+                                color: Colors.white,
+                              ),
+                              label: const Text(
+                                "Reorder",
+                                style: TextStyle(color: Colors.white),
+                              ))
+                        ],
+                      ),
                     ),
                   )
                 ],
               )),
         ),
+        Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Container(
+            height: 60,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              color: Colors.white,
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.3),
+                  spreadRadius: 2,
+                  blurRadius: 5,
+                  offset: Offset(0, 3),
+                ),
+              ],
+            ),
+            child: Padding(
+              padding: const EdgeInsets.only(left: 15),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "You Rated",
+                    style: TextStyle(fontSize: 17, fontWeight: FontWeight.bold),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(right: 15),
+                    child: Row(
+                      children: [
+                        rate_star(Cc: Colors.amber),
+                        rate_star(Cc: Colors.amber),
+                        rate_star(Cc: Colors.amber),
+                        rate_star(Cc: Color.fromARGB(60, 0, 0, 0)),
+                        rate_star(Cc: Color.fromARGB(60, 0, 0, 0)),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        )
       ],
     );
   }
