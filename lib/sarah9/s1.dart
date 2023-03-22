b8fcbd78d365754c9652f449921e503586d4a83b
Sarah Ahmed <sarah991salim@gmail.com>
2023-03-22 23:42:53 +0300

    rr

--- lib/sarah9/s1.dart
+++ lib/sarah9/s1.dart
@@ -16,63 +16,74 @@ class _s1State extends State<s1> {
       backgroundColor: Colors.white24,
       body: SafeArea(
         child: Column(children: [
-          Padding(
-            padding: const EdgeInsets.only(right: 10.0
-
-            ),
-            child: Row(
-              children: [
-              IconButton(onPressed: () {}, icon:const Icon(Icons.menu_rounded,)),
-              Expanded(child: Container(
-                padding: const EdgeInsets.only(left: 10.0),
-                height: 35,
-                decoration: BoxDecoration(
-                  color: Color.fromARGB(96, 239, 236, 236),
-                  borderRadius: BorderRadius.circular(50),
-          
-                ),
-                child: Row(
-                  children: [
-                    Expanded(
-                      child: TextFormField(
-                        decoration:const InputDecoration(
-                          hintText: "Serch for store or an item",
-                          hintStyle: TextStyle(
-                            fontSize: 14,
-                          ),
-                          enabledBorder: InputBorder.none,
-                          focusedBorder: InputBorder.none,
-                        ),
-                    
-                      ),
-          
-                    ),
-                    Container(
-                padding: const EdgeInsets.only(left: 10.0),
-                height: 35,
-                width: 60,
-                decoration: BoxDecoration(
-                  color: Color(0xD112CC1B),
-                  borderRadius: BorderRadius.circular(50),
-          
-                ),
-                child: Icon(
-                  Icons.search,color:Colors.black87,),
-                )
-          
-                  ],
-                ),
-          
-              ))
-          
-          
-          
-          
-              
-            ],),
-          )
+          CustomAppbar()
         ],),
       ),
+    );
+  }
+}
+
+class CustomAppbar extends StatelessWidget {
+  const CustomAppbar({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(right: 10.0
+
+      ),
+      child: Row(
+        children: [
+        IconButton(onPressed: () {}, icon:const Icon(Icons.menu_rounded,)),
+        Expanded(child: Container(
+          padding: const EdgeInsets.only(left: 10.0),
+          height: 35,
+          decoration: BoxDecoration(
+            color: Color.fromARGB(96, 239, 236, 236),
+            borderRadius: BorderRadius.circular(50),
+    
+          ),
+          child: Row(
+            children: [
+              Expanded(
+                child: TextFormField(
+                  decoration:const InputDecoration(
+                    hintText: "Serch for store or an item",
+                    hintStyle: TextStyle(
+                      fontSize: 14,
+                    ),
+                    enabledBorder: InputBorder.none,
+                    focusedBorder: InputBorder.none,
+                  ),
+              
+                ),
+    
+              ),
+              Container(
+          padding: const EdgeInsets.only(left: 10.0),
+          height: 35,
+          width: 60,
+          decoration: BoxDecoration(
+            color: Color(0xD112CC1B),
+            borderRadius: BorderRadius.circular(50),
+    
+          ),
+          child: Icon(
+            Icons.search,color:Colors.black87,),
+          )
+    
+            ],
+          ),
+    
+        ))
+    
+    
+    
+    
+        
+      ],),
     );
   }
 }
