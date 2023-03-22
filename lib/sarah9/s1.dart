076f41d9bb6d1057d5a2d37c8318ebe86c02ba52
Sarah Ahmed <sarah991salim@gmail.com>
2023-03-22 23:29:54 +0300

    u

--- lib/sarah9/s1.dart
+++ lib/sarah9/s1.dart
@@ -50,7 +50,7 @@ class _s1State extends State<s1> {
                 borderRadius: BorderRadius.circular(50),
 
               ),
-              child: Icon(Icons.search),
+              child: Icon(Icons.search,color:Colors.black87,),
               )
 
                 ],
