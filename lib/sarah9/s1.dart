9226aeabe39b606f5a48cc2aa90207b9aea61e1d
Sarah Ahmed <sarah991salim@gmail.com>
2023-03-23 03:07:26 +0300

    padding left

--- lib/sarah9/s1.dart
+++ lib/sarah9/s1.dart
@@ -52,25 +52,28 @@ final String title;
 final VoidCallback press;
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      onTap: press,
-      child: Chip(
-      backgroundColor: Color.fromARGB(255, 13,180,138),
-      shape: RoundedRectangleBorder(
-    
-        borderRadius: BorderRadius.circular(10)
-      ),
-        
-        label: Padding(
-        padding:const
-          EdgeInsets.symmetric(horizontal: 10.0,vertical: 10.0),
-          child: Text(title,
-          style:const TextStyle(
-            color: Colors.black,
-            fontWeight: FontWeight.w600),
+    return Padding(
+      padding: const EdgeInsets.only(left: 10.0),
+      child: InkWell(
+        onTap: press,
+        child: Chip(
+        backgroundColor: Color.fromARGB(255, 13,180,138),
+        shape: RoundedRectangleBorder(
+      
+          borderRadius: BorderRadius.circular(10)
+        ),
+          
+          label: Padding(
+          padding:const
+            EdgeInsets.symmetric(horizontal: 10.0,vertical: 10.0),
+            child: Text(title,
+            style:const TextStyle(
+              color: Colors.black,
+              fontWeight: FontWeight.w600),
+              ),
+          )
             ),
-        )
-          ),
+      ),
     );
   }
 }
